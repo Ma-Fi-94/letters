ef4e9898bc8c49969cfc01b0dae9ab2036094825
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2021-10-17 17:02:26 +0200

    Add files via upload

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ import os
 import requests
 import typing
 
+
 def download(url: str, filename: str) -> None:
     '''Download the website at *url* and save it to *filename*'''
     try:
@@ -10,22 +11,22 @@ def download(url: str, filename: str) -> None:
         with open(filename, 'w') as f:
             f.write(r.text)
     except:
-        print ("Error trying to download " + url + ". Aborting.")
+        print("Error trying to download " + url + ". Aborting.")
         raise SystemExit
 
     size = os.path.getsize(filename)
     if size > 0:
-        print ("Downloaded " + str(size) + " bytes.")
+        print("Downloaded " + str(size) + " bytes.")
     else:
-        print ("Download failed. Aborting.")
+        print("Download failed. Aborting.")
         raise SystemExit
 
 
-def main():
+def main() -> None:
     # First set of chapters
     base_url = "https://www.projekt-gutenberg.org/goethe/brschil1/chap0"
     suffix = ".html"
-    for nb in range(1,6):
+    for nb in range(1, 6):
         url = base_url + str(nb) + suffix
         filename = "./raw/" + str(nb) + ".html"
         download(url, filename)
@@ -33,10 +34,11 @@ def main():
     # Second set of chapters
     base_url = "https://www.projekt-gutenberg.org/goethe/brschil2/chap00"
     suffix = ".html"
-    for nb in range(1,9):
+    for nb in range(1, 9):
         url = base_url + str(nb) + suffix
-        filename = "./raw/" + str(nb+5) + ".html"
+        filename = "./raw/" + str(nb + 5) + ".html"
         download(url, filename)
+
 
 if __name__ == "__main__":
     main()
