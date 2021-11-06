0d7b034592674772464d4f88fdb3438ea07b4524
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2021-11-06 19:50:10 +0100

    Add files via upload

--- lib_processing.py
+++ lib_processing.py
@@ -2,20 +2,16 @@ from sklearn.feature_extraction.text import CountVectorizer
 import sklearn.metrics.pairwise
 
 def cosinesim(X):
-    # Compute cosine similarity matrix on the individual letters (i.e. vectors)
+    ''' Compute cosine similarity matrix on the individual letters (i.e. bag-of-words vectors in X) '''
     cosinesim = sklearn.metrics.pairwise.cosine_similarity(X)
     return cosinesim
 
 
 def bag_of_words(df, n_gram_n = 1, min_word_length = 3, normalise_counts = False, binary = False):
-    # We will fit a bag of words model with n-grams, consisting of l+ letters only
-    
+    ''' Fit a bag of words model with n_gram_n-grams, consisting of min_word_length+ letters only. '''
     # normalise_counts: Norm word counts across the letters?
-    
     # binary: Binary presence-absence data instead of frequencies?
-    # Apparently, this does not alter results qualitatively,
-    # regardless of whether wie normalise word counts or not
-    
+
     # Model fitting
     token_pattern = "[a-zA-Z]{" + str(min_word_length) + ",100000}"
     model = CountVectorizer(ngram_range=(n_gram_n,n_gram_n), token_pattern=token_pattern, binary=binary)
